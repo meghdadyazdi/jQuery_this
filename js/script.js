c0ef26eaa43b69e7c8dd597a2175e509eeeb0ab3
MegY <meghdadyazdi@yahoo.com>
2019-11-22 19:06:25 +0000

    Adding jQuery

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// // $(document).ready(function(){
+// $('p').click(function () {
+//     $(this).slideToggle('slow')
+// })
+// // })
+
+// --------------------------------------------------------------
+
+//     $('button').mouseenter(function () {
+//         $(this).removeClass('makeRed').addClass('makeBlue')
+//     })
+
+// $('button').mouseleave(function () {
+//     $(this).removeClass('makeBlue').addClass('makeRed')
+// })
+
+// --------------------------------------------------------------
+
+    // $(document).ready(function () {
+    //     $('.box').click(function () {
+    //         var classNames=$(this).attr('class').split(' ')
+    //         $('.'+classNames[1]).css('backgroung-color', 'red')
+    //     });
+    // });
+
+
+    $(document).ready(function () {
+        $(".box").click(function () {
+            var classNames=$(this).attr("class").split(" ");
+            $("." + classNames[1]).css("backgroung-color", "red");
+        });
+    });
